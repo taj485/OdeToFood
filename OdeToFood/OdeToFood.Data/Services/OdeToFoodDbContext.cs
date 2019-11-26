ed0c14696810b584cd0c1bee5726f49386fab066
Taj485 <tajbi21@gmail.com>
2019-11-26 01:28:47 +0000

    Created database that can store restaurantmodel data

---
+++ OdeToFood/OdeToFood.Data/Services/OdeToFoodDbContext.cs
@@ -0,0 +1,13 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace OdeToFood.Data.Services
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<RestaurantModel> Restaurants { get; set; }
+    }
+}
